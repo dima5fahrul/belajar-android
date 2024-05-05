9bb24b1778fd29eb9cccb3bf78373c7706a529f8
Dimas Fahrul <dfahrul07@gmail.com>
2024-05-05 21:22:33 +0700

    init: new project

---
+++ app/src/main/java/com/example/belajar_android/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.belajar_android
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var sayHelloButton: Button
+    private lateinit var sayHelloTextView: TextView
+
+    private fun initComponents() {
+        nameEditText = findViewById(R.id.nameEditText)
+        sayHelloButton = findViewById(R.id.sayHelloButton)
+        sayHelloTextView = findViewById(R.id.sayHelloText)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hello_world)
+
+        initComponents()
+
+        sayHelloTextView.text = "Hello World!"
+
+        sayHelloButton.setOnClickListener {
+            val name = nameEditText.text.toString()
+            sayHelloTextView.text = "Hello $name!"
+        }
+    }
+}
